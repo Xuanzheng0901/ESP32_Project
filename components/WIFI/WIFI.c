e0396d12e9579e72754081c6a6719bb1faff322b
Xuanzheng0901 <szx1691196289@gmail.com>
2025-02-01 17:51:51 +0800

    更新wifi驱动

--- components/WIFI/WIFI.c
+++ components/WIFI/WIFI.c
@@ -17,6 +17,14 @@
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
+const char *ipv6_str[6] = {
+    "ESP_IP6_ADDR_IS_UNKNOWN",
+    "ESP_IP6_ADDR_IS_GLOBAL",
+    "ESP_IP6_ADDR_IS_LINK_LOCAL",
+    "ESP_IP6_ADDR_IS_SITE_LOCAL",
+    "ESP_IP6_ADDR_IS_UNIQUE_LOCAL",
+    "ESP_IP6_ADDR_IS_IPV4_MAPPED_IPV6"
+};
 
 esp_event_handler_instance_t instance_any_id;
 esp_event_handler_instance_t instance_got_ip;
@@ -31,7 +39,11 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
     {
         esp_wifi_connect();
-    } 
+    }
+    else if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_CONNECTED)
+    {
+        esp_netif_create_ip6_linklocal((esp_netif_t*)arg);
+    }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) 
     {
         WIFI_STATU = 0;
@@ -48,8 +60,11 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
     }
     else if(event_id == IP_EVENT_GOT_IP6)
     {
-        for(int i = 0; i < 4; i++)
-            printf("%lx ", ((ip_event_got_ip6_t*)event_data)->ip6_info.ip.addr[i]);
+        ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
+        esp_ip6_addr_type_t ipv6_type = esp_netif_ip6_get_addr_type(&event->ip6_info.ip);
+        ESP_LOGI(TAG, "Got IPv6 event: Interface \"%s\" address: " IPV6STR ", type: %s", esp_netif_get_desc(event->esp_netif),
+                IPV62STR(event->ip6_info.ip), ipv6_str[ipv6_type]);
+        ESP_LOGI(TAG, "- IPv6 address: " IPV6STR ", type: %s", IPV62STR(event->ip6_info.ip), ipv6_str[ipv6_type]);
     }
 }
 
@@ -67,17 +82,25 @@ void wifi_init_sta(void)
     esp_netif_init();
     
     netif_handle = esp_netif_create_default_wifi_sta();
-    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id);
-    esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_got_ip);
+
+    esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &event_handler, netif_handle);
+    esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &event_handler, NULL);
+    esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_START, &event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "Xiaomi 14 Pro",
-            .password = "12345678.",
-            .threshold.authmode = WIFI_AUTH_WPA2_WPA3_PSK,
-            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+            .ssid = "CU_QE6h",
+            .password = "szx123456",
+            .threshold.authmode = WIFI_AUTH_WPA_WPA2_PSK,
         },
+        // .sta = {
+        //     .ssid = "Xiaomi 14 Pro",
+        //     .password = "12345678.",
+        //     .threshold.authmode = WIFI_AUTH_WPA2_WPA3_PSK,
+        //     .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        // },
     };
 
     esp_wifi_init(&cfg);
